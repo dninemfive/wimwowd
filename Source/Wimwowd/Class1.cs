8daa3548f5cf8c8f9375f50580c0171bba2db1a6
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-04-23 16:25:51 -0700

    do wowk

--- Source/Wimwowd/Class1.cs
+++ Source/Wimwowd/Class1.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
 
 namespace Wimwowd
 {
-    public class Class1
+    [StaticConstructorOnStartup]
+    public class HawmonyPatch
     {
+        static HawmonyPatch()
+        {
+            Harmony hawmony = new Harmony("com.dninemfive.ohgod");
+            hawmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Translator), "Translate", new Type[] { typeof(string) })]
+        class UwUpatch
+        {
+            [HarmonyPostfix]
+            public void TranslatePostfix(ref TaggedString __result)
+            {
+                // __result = __result.Replace("[lr]", "w").Replace("[LR]", "W").Replace("WW", "W").Replace("[Ww]w", "w"); // string.Replace doesn't support regex smdh
+                __result = __result.Replace("l", "w")
+                                   .Replace("r", "w")
+                                   .Replace("L", "W")
+                                   .Replace("R", "W")
+                                   .Replace("WW", "W")
+                                   .Replace("Ww", "W")
+                                   .Replace("wW", "w")
+                                   .Replace("ww", "w");
+            }
+        }
     }
 }
